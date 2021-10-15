42af1bd2605c36bcf8e47c4a34b3515d64539bde
Lucas Hermus <lucas.hermus@student.gildeopleidingen.nl>
2021-10-15 10:37:25 +0200

    small changes
    small changes

--- Sun Tours-Website/php/Review.php
+++ Sun Tours-Website/php/Review.php
@@ -69,7 +69,7 @@ $SESSION['userClass']->showReview(false);
                 </li>
                 <li class="nav-item" id='logout_li'>
           <form method="POST" id="logoutForm"  >
-            <input type="submit" id="logout" name="logout" value="Logout" class="btn btn-sm btn-outline-secondary">
+            <input type="submit" id="logout" name="logout" value="loguit" class="btn btn-sm btn-outline-secondary">
           </form>
         </li>
             </ul>
