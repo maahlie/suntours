712485af4e925ef2a0545efd6d7545d7b0403ad0
Stef Berben <Stef.Berben@Student.GildeOpleidingen.nl>
2021-11-01 09:08:32 +0100

    css aanpassingen

--- Sun Tours-Website/php/Review.php
+++ Sun Tours-Website/php/Review.php
@@ -13,6 +13,7 @@ $SESSION['userClass']->showReview(false);
     <title>Sun Tours</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/styles.css">
     <link rel="stylesheet" href="../css/Review_Cards.css">
@@ -27,18 +28,17 @@ $SESSION['userClass']->showReview(false);
     <script src="node_modules/jquery/dist/jquery.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,200&display=swap" rel="stylesheet"> 
-    <link rel="stylesheet" type="text/css"
-        href="https://cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick-theme.css" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,200&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick-theme.css" />
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
     <script type="text/javascript" src="https://cdn.weglot.com/weglot.min.js"></script>
-  <script>
-    Weglot.initialize({
-        api_key: 'wg_e0862d6fccddf79b84908b2ed4ee35a62'
-    });
-</script>
+    <script>
+        Weglot.initialize({
+            api_key: 'wg_e0862d6fccddf79b84908b2ed4ee35a62'
+        });
+    </script>
 </head>
 
 <body class="Review_Background">
@@ -68,13 +68,13 @@ $SESSION['userClass']->showReview(false);
                     <a class="nav-link" href="./Review.php">Review</a>
                 </li>
                 <li class="nav-item">
-          <a class="nav-link" href="boekingsGeschiedenis.php">boekingen overzicht</a>
-        </li>
+                    <a class="nav-link" href="boekingsGeschiedenis.php">boekingen overzicht</a>
+                </li>
                 <li class="nav-item" id='logout_li'>
-          <form method="POST" id="logoutForm"  >
-            <input type="submit" id="logout" name="logout" value="Log uit" class="btn btn-sm btn-outline-secondary">
-          </form>
-        </li>
+                    <form method="POST" id="logoutForm">
+                        <input type="submit" id="logout" name="logout" value="Log uit" class="btn btn-sm btn-outline-secondary">
+                    </form>
+                </li>
             </ul>
         </div>
     </nav>
@@ -87,7 +87,7 @@ $SESSION['userClass']->showReview(false);
 
             <form method="POST" id="reviewForm">
                 <div class="review-form">
-<br>
+                    <br>
                     <b> U moet ingelogd zijn om een review te kunnen schrijven.</b><br><br>
                     <label for="Holiday">Welke vakantie wilt u reviewen:</label><br>
                     <select name="holidays" class="select" id="holidays">
@@ -108,26 +108,16 @@ $SESSION['userClass']->showReview(false);
                                 <div class="card">
 
                                     <fieldset class="rating">
-                                        <input type="radio" id="star5" name="rating" value="5" /><label class="full"
-                                            for="star5" title="Perfect - 5 stars"></label>
-                                        <input type="radio" id="star4half" name="rating" value="4.5" /><label
-                                            class="half" for="star4half" title="Erg goed - 4.5 stars"></label>
-                                        <input type="radio" id="star4" name="rating" value="4" /><label class="full"
-                                            for="star4" title="Goed - 4 stars"></label>
-                                        <input type="radio" id="star3half" name="rating" value="3.5" /><label
-                                            class="half" for="star3half" title="Voldoende - 3.5 stars"></label>
-                                        <input type="radio" id="star3" name="rating" value="3" /><label class="full"
-                                            for="star3" title="Goed genoeg - 3 stars"></label>
-                                        <input type="radio" id="star2half" name="rating" value="2.5" /><label
-                                            class="half" for="star2half" title="Kan beter - 2.5 stars"></label>
-                                        <input type="radio" id="star2" name="rating" value="2" /><label class="full"
-                                            for="star2" title="Beetje slecht - 2 stars"></label>
-                                        <input type="radio" id="star1half" name="rating" value="1.5" /><label
-                                            class="half" for="star1half" title="Slecht- 1.5 stars"></label>
-                                        <input type="radio" id="star1" name="rating" value="1" /><label class="full"
-                                            for="star1" title="heel erg slecht - 1 star"></label>
-                                        <input type="radio" id="starhalf" name="rating" value="0.5" /><label
-                                            class="half" for="starhalf" title="Verschrikkelijk - 0.5 stars"></label>
+                                        <input type="radio" id="star5" name="rating" value="5" /><label class="full" for="star5" title="Perfect - 5 stars"></label>
+                                        <input type="radio" id="star4half" name="rating" value="4.5" /><label class="half" for="star4half" title="Erg goed - 4.5 stars"></label>
+                                        <input type="radio" id="star4" name="rating" value="4" /><label class="full" for="star4" title="Goed - 4 stars"></label>
+                                        <input type="radio" id="star3half" name="rating" value="3.5" /><label class="half" for="star3half" title="Voldoende - 3.5 stars"></label>
+                                        <input type="radio" id="star3" name="rating" value="3" /><label class="full" for="star3" title="Goed genoeg - 3 stars"></label>
+                                        <input type="radio" id="star2half" name="rating" value="2.5" /><label class="half" for="star2half" title="Kan beter - 2.5 stars"></label>
+                                        <input type="radio" id="star2" name="rating" value="2" /><label class="full" for="star2" title="Beetje slecht - 2 stars"></label>
+                                        <input type="radio" id="star1half" name="rating" value="1.5" /><label class="half" for="star1half" title="Slecht- 1.5 stars"></label>
+                                        <input type="radio" id="star1" name="rating" value="1" /><label class="full" for="star1" title="heel erg slecht - 1 star"></label>
+                                        <input type="radio" id="starhalf" name="rating" value="0.5" /><label class="half" for="starhalf" title="Verschrikkelijk - 0.5 stars"></label>
                                     </fieldset>
                                 </div>
                             </div>
@@ -147,43 +137,42 @@ $SESSION['userClass']->showReview(false);
                     <label>Zou u deze website aanbevelen?</label>
                     <br>
                     <!-- <form class="RadioButton"> -->
-                    
-                        <input type="radio" id="keuze" name="keuze" value="ja">
-                        <label for="ja">Ja</label><br>
-                        <input type="radio" id="keuze2" name="keuze" value="nee">
-                        <label for="nee">Nee</label><br>
-                        <br>
-                        <input type="submit" class="buttonReview" name="sendReview" value="verstuur">
+
+                    <input type="radio" id="keuze" name="keuze" value="ja">
+                    <label for="ja">Ja</label><br>
+                    <input type="radio" id="keuze2" name="keuze" value="nee">
+                    <label for="nee">Nee</label><br>
+                    <br>
+                    <input type="submit" class="buttonReview" name="sendReview" value="verstuur">
 
                     <!-- </form> -->
                 </div>
 
             </form>
 
-            <div class="items" >
+            <div class="items">
                 <?php
-                for($i = 1; $i < ($SESSION['userClass']->reviewLength)+1; $i++){
+                for ($i = 1; $i < ($SESSION['userClass']->reviewLength) + 1; $i++) {
                     if ($SESSION['userClass']->arrayReview[$i][2])
-                    ?>
-                <div class="card" id='review'>
-                    <div class="card-body" id='review_<?php echo $i ?>' >
-                        <h4 class="card-title"><img src="https://img.icons8.com/ultraviolet/40/000000/quote-left.png" > </h4>
-                        <div class="template-demo">
-                            <p><?php echo $SESSION['userClass']->arrayReview[$i][1] ?></p>
-                        </div>
-                        <hr>
-                        <div class="row">
-                            <div class="col-sm-2"> <img class="profile-pic"
-                                    src="https://img.icons8.com/bubbles/100/000000/edit-user.png"> </div>
-                            <div class="col-sm-10">
-                                <div class="profile">
-                                    <h4 class="cust-name"><?php echo $SESSION['userClass']->arrayReview[$i][2] ?></h4>
-                                    <p class="cust-profession"><?php echo $SESSION['userClass']->arrayReview[$i][3] ?></p>
+                ?>
+                    <div class="card" id='review'>
+                        <div class="card-body" id='review_<?php echo $i ?>'>
+                            <h4 class="card-title"><img src="https://img.icons8.com/ultraviolet/40/000000/quote-left.png"> </h4>
+                            <div class="template-demo">
+                                <p><?php echo $SESSION['userClass']->arrayReview[$i][1] ?></p>
+                            </div>
+                            <hr>
+                            <div class="row">
+                                <div class="col-sm-2"> <img class="profile-pic" src="https://img.icons8.com/bubbles/100/000000/edit-user.png"> </div>
+                                <div class="col-sm-10">
+                                    <div class="profile">
+                                        <h4 class="cust-name"><?php echo $SESSION['userClass']->arrayReview[$i][2] ?></h4>
+                                        <p class="cust-profession"><?php echo $SESSION['userClass']->arrayReview[$i][3] ?></p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
                 <?php
                 }
                 ?>
